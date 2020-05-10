b340aff0423f3e4b43040b4dde755d0247911a1e
petermadale <petermadale@turnkeyinc.com>
2020-05-10 11:16:14 +0800

    redeploy 2

--- src/server/server.js
+++ src/server/server.js
@@ -9,7 +9,7 @@ import { connect } from "mongodb";
 import path from "path";
 
 //let port = process.env.port || 7777;
-let port = process.env.port;
+let port = process.env.PORT;
 let app = express();
 
 app.listen(port, console.log("Server listening on port", port));
