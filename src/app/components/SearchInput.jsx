ac1659cc0eb42f0df3bb78e9e45fbe74bd9001ea
petermadale <petermadale@turnkeyinc.com>
2020-05-11 16:51:45 +0800

    please na

--- src/app/components/SearchInput.jsx
+++ src/app/components/SearchInput.jsx
@@ -1,22 +1,15 @@
 import React from "react";
 import { connect } from "react-redux";
-import { Autocomplete } from "react-autocomplete";
 
-export const SearchInput = () => (
-  <form className="form-inline ml-3">
-    <div className="input-group input-group-sm">
-      <input
-        className="form-control form-control-navbar"
-        type="search"
-        placeholder="Search Client"
-        aria-label="Search"
-      />
-      <div className="input-group-append">
-        <button className="btn btn-navbar" type="submit">
-          <i className="fas fa-search"></i>
-        </button>
-      </div>
-    </div>
+export const SearchInput = ({ id, name }) => (
+  <form>
+    <input
+      type="text"
+      name=""
+      id=""
+      className="form-control"
+      placeholder="Search Client"
+    />
   </form>
 );
 
